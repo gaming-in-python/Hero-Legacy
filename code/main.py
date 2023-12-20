f6fd4f1877cc14c7c087845d50bb5681435881d9
anilachundi <achundi2@illinois.edu>
2023-12-20 15:18:53 -0800

    Adding level class

--- code/main.py
+++ code/main.py
@@ -1,15 +1,15 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 class Game:
 	def __init__(self):
 		  
 		# general setup
 		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+		self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
 		pygame.display.set_caption('Game')
 		self.clock = pygame.time.Clock()
-	
+		self.level = Level()
 	def run(self):
 		while True:
 			for event in pygame.event.get():
@@ -18,6 +18,7 @@ class Game:
 					sys.exit()
 
 			self.screen.fill('black')
+			self.level.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
 
