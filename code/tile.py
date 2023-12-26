ee4d14c77117fc0b61657454e6f3814f2fec01cd
hryding444 <hryding2@illinois.edu>
2023-12-26 11:38:40 -0600

    added main camera function, weird bug where player teleports

--- code/tile.py
+++ code/tile.py
@@ -1,8 +1,10 @@
 import pygame
 from settings import *
+import os
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
+        # print("Current Working Directory:", os.getcwd())
         self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
