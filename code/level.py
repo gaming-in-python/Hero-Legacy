f0ff0bed81acfd0e9d8ef97b861effb1393c2def
Navomi2 <nbyju2@illinois.edu>
2023-12-29 16:40:52 -0600

    new map in background

--- code/level.py
+++ code/level.py
@@ -1,32 +1,62 @@
-import pygame
+import pygame 
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
+from support import *
+from random import choice
 class Level:
-    def __init__(self):
+	def __init__(self):
 
-        #getting display surface
-        self.display_surface = pygame.display.get_surface()
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
 
-        self.visibles = pygame.sprite.Group()
-        self.obstacles = pygame.sprite.Group()
+		# sprite group setup
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
 
-        self.create_map()
+		# sprite setup
+		self.create_map()
 
-    def create_map(self):     
-        for row in range(0, HEIGHT, TILESIZE):
-            for col in range(0, WIDTH, TILESIZE):
-                tile_value = WORLD_MAP[row // TILESIZE][col // TILESIZE]
-                x = col
-                y = row
-                if tile_value == 'x':
-                    Tile((x,y),[self.visibles, self.obstacles])
-                if tile_value == 'p':
-                    Player((x,y),[self.visibles])
+	def create_map(self):
+		for row_index,row in enumerate(WORLD_MAP):
+			for col_index, col in enumerate(row):
+				x = col_index * TILESIZE
+				y = row_index * TILESIZE
+				if col == 'x':
+					Tile((x,y), [self.visible_sprites,self.obstacle_sprites])
+				if col == 'p':
+					self.player = Player((x,y),[self.visible_sprites,self.obstacle_sprites])
 
+	def run(self):
+		# update and draw the game
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
 
+class YSortCameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		# general setup 
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
 
-    def run(self):
-        self.visibles.draw(self.display_surface)
+		# creating the floor: loading png for background then place at (0,0)
+		self.floor_surf = pygame.image.load('../graphics/tilemap/zelda_ground2.png').convert()
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
+	def custom_draw(self,player):
+		# getting the offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
+
+		# drawing the floor (this is background image so it must be done before sprites)
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf,floor_offset_pos)
+
+		# for sprite in self.sprites():
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image,offset_pos)
     
--- code/player.py
+++ code/player.py
@@ -1,8 +1,41 @@
-import pygame
+import pygame 
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
-        super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-26)
+
+		self.direction = pygame.math.Vector2()
+		self.speed = 5
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+	def move(self,speed):
+		if self.direction.magnitude() != 0:
+			self.direction = self.direction.normalize()
+
+		self.hitbox.x += self.direction.x * speed
+		self.hitbox.y += self.direction.y * speed
+		self.rect.center = self.hitbox.center
+
+	def update(self):
+		self.input()
+		self.move(self.speed)
